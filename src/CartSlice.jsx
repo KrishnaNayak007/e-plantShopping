dcb9d30158c022004bf2dd682edd872368c1a1f3
KrishnaNayak007 <krrishnayak144@gmail.com>
2025-07-20 15:38:23 +0530

    update CartSlice.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -3,17 +3,28 @@ import { createSlice } from '@reduxjs/toolkit';
 export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
-    items: [], // Initialize items as an empty array
+    items: [], 
   },
   reducers: {
     addItem: (state, action) => {
+      const {name,image,cost} = action.payload;
+      const existingItem = state.items.find(item => item.name === action.payload.name);
+      if (existingItem) {
+        existingItem.quantity += 1; // Increment quantity if item already exists
+      } else {
+        state.items.push({ ...action.payload, quantity: 1 }); // Add new item with quantity 1
+      }
     
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter(item => item.name !== action.payload.name);
     },
     updateQuantity: (state, action) => {
-
-    
+      const { name, quantity } = action.payload;
+      const itemToUpdate = state.items.find(item => item.name === name);
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity; 
+      }
     },
   },
 });
